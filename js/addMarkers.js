28fc18166a40c7c3d8153de0d5e600be5c2fb1b6
MasterYeeter42 <108326655+MasterYeeter42@users.noreply.github.com>
2024-04-17 17:55:58 +0100

    Update addMarkers.js

--- js/addMarkers.js
+++ js/addMarkers.js
@@ -1,5 +1,31 @@
 AFRAME.registerComponent("create-markers", {
   
   //Add Code Here!
-  
-  });
+  init: async function() {
+
+    var mainScene = document.querySelector("#main-scene");
+
+    var dishes = await this.getDishes();
+
+    dishes.map(dish => {
+      var marker = document.createElement("a-marker");
+      marker.setAttribute("id", dish.id);
+      marker.setAttribute("type", "pattern");
+      marker.setAttribute("url", dish.marker_pattern_url);
+      marker.setAttribute("cursor", {
+        rayOrigin: "mouse"
+      });
+
+      marker.setAttribute("markerhandler", {});
+      mainScene.appendChild(marker);
+
+      var model = document.createElement("a-entity");
+
+      model.sertAttribute("id", `model-${dish.id`);
+      model.sertAttribute("position", dish.model_geometry.position);
+      model.sertAttribute("rotation", dish.model_geometry.position);
+      model.sertAttribute("scale", dish.model_geometry.scale);
+      model.sertAttribute("gesture-handler", );
+    });
+  };
+});
